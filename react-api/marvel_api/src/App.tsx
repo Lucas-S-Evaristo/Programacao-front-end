59f2a11e80307b860eb522dcee5b79e0f82c8f80
Lucas Evaristo <lucasev35@gmail.com>
2022-05-02 17:30:26 -0300

    Commit Inicial

--- react-api/marvel_api/src/App.tsx
+++ react-api/marvel_api/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import md5 from 'md5';
+import { error } from 'console';
 
-function App() {
+/* http://gateway.marvel.com/v1/public/comics? */
+
+const baseUrl = 'http://gateway.marvel.com/v1/public/characters?'
+const publicKey = '8e858de7ce67d5d86d38b7541ebf0437'
+const privateKey = '7cd1fef9ca2ce802909d3f1c517d0ddb9f5e1f42'
+
+const time = Number(new Date())
+console.log(time);
+
+/* baseUrl + 'ts=' + time + '&apikey=' + publicKey + '&hash=' + hash */
+
+const hash = md5(time + privateKey + publicKey)
+
+const App: React.FC = () => {
+  useEffect(() => {
+    /* axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`).then().catch() */
+    axios
+    .get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`)
+    .then(resposta => console.log(resposta))
+    .catch(error => console.log (error))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      API da Marvel
     </div>
   );
 }
